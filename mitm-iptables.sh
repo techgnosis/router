d90f4cfa76dab1a4712be93d98b591a161c3f698
James Musselwhite <james.musselwhite@gmail.com>
2025-04-26 14:29:46 -0700

    mitm iptables rules

---
+++ mitm-iptables.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+iptables \
+--table nat \
+--flush PREROUTING
+
+iptables \
+--table nat \
+--append PREROUTING \
+--in-interface br0 \
+--protocol tcp \
+--destination-port 80 \
+--jump REDIRECT \
+--to-port 8080
+
+iptables \
+--table nat \
+--append PREROUTING \
+--in-interface br0 \
+--protocol tcp \
+--destionation-port 443 \
+--jump REDIRECT \
+--to-port 8080
