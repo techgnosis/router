b20ef1ff1c259d44aa04b9cde73caac5d71f24de
James Musselwhite <james.musselwhite@gmail.com>
2025-04-26 12:24:03 -0700

    iptables

--- up.sh
+++ up.sh
@@ -38,11 +38,3 @@ systemctl start hostapd
 # routes
 
 ip route add 0.0.0.0/0 via 192.168.0.1
-
-
-# iptables
-
-iptables -t nat -F
-
-iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o enp0s31f6 -j MASQUERADE
-
