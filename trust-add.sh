c7636b7183ba75fc29b9f20f9dd9f3fd179f3909
James Musselwhite <james.musselwhite@gmail.com>
2025-04-26 16:50:21 -0700

    mitm complete

--- trust-add.sh
+++ trust-add.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-sudo security add-trusted-cert -d -p ssl -p basic -k /Library/Keychains/System.keychain mitmproxy-ca-cert.pem
+sudo security add-trusted-cert -d -p ssl -p basic mitmproxy-ca-cert.pem
---
+++ trust-dump.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+sudo security dump-trust-settings -d
