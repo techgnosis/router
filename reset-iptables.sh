2ae0cf2d729c40a75b9936b0ea23b1451c2fddd7
James Musselwhite <james.musselwhite@gmail.com>
2025-04-27 15:47:18 -0700

    reset

---
+++ reset-iptables.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+set -euxo pipefail
+
+
+iptables --policy INPUT ACCEPT
+iptables --policy FORWARD ACCEPT
+iptables --policy OUTPUT ACCEPT
+
+iptables --table filter --flush
+iptables --table nat --flush
+
+iptables --table filter --delete-chain
+iptables --table nat --delete-chain
